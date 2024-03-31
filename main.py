bcf78ff25f4cc06d9a96d428742e68550d00ab72
Cedric Men <c.m.cedricm17@gmail.com>
2024-03-31 03:31:31 -0700

    connected system to postgresql database instead, migrated to postgresql

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 import googleapiclient.discovery as gAPI
-import json, os
+from prisma import Prisma, Client
+import json, os, argparse, asyncio
+
 load_dotenv()
 
 api_service_name = os.getenv("GOOGLE_API_SERVICE")
@@ -8,6 +10,8 @@ api_version = os.getenv("GOOGLE_API_VERSION")
 DEVELOPER_KEY = os.getenv("GOOGLE_API_KEY")
 
 youtube = gAPI.build(api_service_name, api_version, developerKey=DEVELOPER_KEY)
+
+prisma = Prisma()
 
 def getVideoURLs(videoData: list) -> list[str]: 
     videoTags: list[str] = []
@@ -17,25 +21,49 @@ def getVideoURLs(videoData: list) -> list[str]:
     return videoTags     
 
 
-def main():
-    videoTagsData: list[str] = []
-    playlists = youtube.playlistItems().list(part=['id','status', "contentDetails"],playlistId='PLwnRU_LD0h-vPfaupYzw5L4AVGB-vj_Kf')
-    data = playlists.execute()
-    videoTagsData.extend(getVideoURLs(data["items"]))
+async def fetchUserPlaylist(UserID: int): 
+    userInfo = await prisma.user.find_unique(where={'id' : int(UserID)}, include={'DataOwnership': True})
+    return userInfo
+
+async def main():
+    await prisma.connect()
+    
+    parser = argparse.ArgumentParser(description="A Program to Grab Information from a Youtube Playlist")
+    parser.add_argument('--userId', type=str, default=None, help="User id from postrgres database")
+    args = parser.parse_args()
+    USER_ID = args.userId
+    
+    if(not USER_ID): 
+       print("Error USER_ID ==> No USER_ID provided in args\n Proper usage: python3 ./main.py --userId  <userID>")
+       exit(-1)
+       
+    userData = await fetchUserPlaylist(USER_ID)
+    if(not userData): 
+        print(f"Error ==> User with ID {USER_ID} could not be found.")
+        exit(-1)
+   
+    PLAYLIST_IDS = await prisma.userplaylist.find_many(where={'dataOwnershipId': int(userData.DataOwnership.id)})
+    
+    for i in range(len(PLAYLIST_IDS)):
+        current_playlist = PLAYLIST_IDS[i].PlayListURL
+        videoTagsData: list[str] = []
+        playlists = youtube.playlistItems().list(part=['id','status', "contentDetails"],playlistId=current_playlist)
+        data = playlists.execute()
+        videoTagsData.extend(getVideoURLs(data["items"]))
 
 
-    nextPageData = data["nextPageToken"] if "nextPageToken" in data else None
-    while(nextPageData): 
-        dataRequest = youtube.playlistItems().list(part=['id','status', "contentDetails"],playlistId='PLwnRU_LD0h-vPfaupYzw5L4AVGB-vj_Kf', pageToken=nextPageData)
-        dataResponse = dataRequest.execute()
-        if "nextPageToken" in dataResponse: 
-            nextPageData = dataResponse["nextPageToken"] 
-        else: nextPageData = None
-        videoTagsData.extend(getVideoURLs(dataResponse['items']))
-        
-    print("Video Tags: ", videoTagsData)
+        nextPageData = data["nextPageToken"] if "nextPageToken" in data else None
+        while(nextPageData): 
+            dataRequest = youtube.playlistItems().list(part=['id','status', "contentDetails"],playlistId=current_playlist, pageToken=nextPageData)
+            dataResponse = dataRequest.execute()
+            if "nextPageToken" in dataResponse: 
+                nextPageData = dataResponse["nextPageToken"] 
+            else: nextPageData = None
+            videoTagsData.extend(getVideoURLs(dataResponse['items']))
+            
+        print("Video Tags: ", videoTagsData)
     
 if __name__ == "__main__": 
-    main()
+    asyncio.run(main())
 
 
